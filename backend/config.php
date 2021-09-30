6ce0b24b3b2f9dab041343c68cda144c8809b95b
Azer_Api <71122219+jarivankaam@users.noreply.github.com>
2021-09-30 11:22:42 +0200

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -4,10 +4,6 @@
 //Hernoem dit bestand naar 'config.php' en vul jouw eigen database-gegevens in.
 //Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
 
-$dbHost = 'rdbms.strato.de';
-$dbName = 'dbs4257837';
-$dbUser = 'dbu2506211';
-$dbPass = 'AzerAapi2020!';
 
 //De url waarop jouw project staat. Géén slash aan het einde.
 $base_url = 'http://azerapi.xyz/';
